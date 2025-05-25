90e69fa185b4d585e9dbbc080c1fc92d4aaf7423
Andrew Osei <andrewosei94@gmail.com>
2025-05-25 18:22:26 +0000

    fixed error in finance calculator - cast input to float instead of int

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income = input("Enter your monthly income:")
 monthly_expenses = input("Enter your total monthly expenses:")
 
-monthly_savings = int(monthly_income) - int(monthly_expenses)
+monthly_savings = float(monthly_income) - float(monthly_expenses)
 
 annual_interest_rate = 0.05
 
