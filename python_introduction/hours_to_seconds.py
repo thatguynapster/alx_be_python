cf123f1d34b06fb864b99c7b4abafd82abfff40f
Andrew Osei <andrewosei94@gmail.com>
2025-05-25 18:01:51 +0000

    corrected error in calculating seconds from hours

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,9 +1,5 @@
 hours = 2
 
-minutes_in_hour = 60
-seconds_in_minute = 60
-
-
-seconds = hours * minutes_in_hour * seconds_in_minute
+seconds = hours * 3600
 
 print(hours, "hour(s) is", seconds, "seconds.")
