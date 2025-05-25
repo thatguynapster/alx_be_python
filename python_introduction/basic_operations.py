173bef852891f08b208ac48e2866401beff9bbce
Andrew Osei <andrewosei94@gmail.com>
2025-05-25 17:45:39 +0000

    basic operations assignment complete

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print("Addition of 10 and 5 is", addition)
+print("Subtraction of 10 and 5 is", subtraction)
+print("Multiplication of 10 and 5 is", multiplication)
