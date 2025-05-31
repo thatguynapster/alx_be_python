4717e5e5ce73abac0110d377fff1b68e5668834f
Andrew Osei <andrewosei94@gmail.com>
2025-05-31 19:51:06 +0000

    fixed error in weather advice - use print in each condition block

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,10 @@
 weather = input("What's the weather like today? (sunny/rainy/cold):")
 
 if weather == "sunny":
-    message = "Wear a t-shirt and sunglasses."
+    print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
-    message = " Don't forget your umbrella and a raincoat."
+    print(" Don't forget your umbrella and a raincoat.")
 elif weather == "cold":
-    message = "Make sure to wear a warm coat and a scarf."
+    print("Make sure to wear a warm coat and a scarf.")
 else:
-    message = "Sorry, I don't have recommendations for this weather."
-
-print(message)
+    print("Sorry, I don't have recommendations for this weather.")
