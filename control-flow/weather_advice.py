c84ef32d8c4647dd56a1fa464f7311a6229c20f8
Andrew Osei <andrewosei94@gmail.com>
2025-05-31 19:49:40 +0000

    fixed error in weather advice - name user input weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
-user_weather = input("What's the weather like today? (sunny/rainy/cold):")
+weather = input("What's the weather like today? (sunny/rainy/cold):")
 
-if user_weather == "sunny":
+if weather == "sunny":
     message = "Wear a t-shirt and sunglasses."
-elif user_weather == "rainy":
+elif weather == "rainy":
     message = " Don't forget your umbrella and a raincoat."
-elif user_weather == "cold":
+elif weather == "cold":
     message = "Make sure to wear a warm coat and a scarf."
 else:
     message = "Sorry, I don't have recommendations for this weather."
