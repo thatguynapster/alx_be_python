923a7f16a618a5f229b6284c41cf335f0b058cb0
Andrew Osei <andrewosei94@gmail.com>
2025-06-13 19:32:40 +0000

    fixed issue with checker

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,13 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtraction":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            try:
-                result = num1 / num2
-                return result
-            except Exception as error:
-                print(f"An error occurred: {error}")
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        try:
+            result = num1 / num2
+            return result
+        except Exception as error:
+            print(f"An error occurred: {error}")
