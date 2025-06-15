b160672694d25181201ee9abe79b1b83049b7fc7
Andrew Osei <andrewosei94@gmail.com>
2025-06-15 21:04:56 +0000

    bank account assignment complete

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+class BankAccount:
+    """docstring for BankAccount."""
+
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        self.account_balance = amount
+
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
